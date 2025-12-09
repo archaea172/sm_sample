5cf53969bc020c8ed14b7741f4a26ac1c127424f
aky <akiyugata271@gmail.com>
2025-12-09 21:41:33 +0900

    fix

--- include/sm_sample/orthogonals/or_joy_monitor.hpp
+++ include/sm_sample/orthogonals/or_joy_monitor.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <smacc2/smacc.hpp>
-#include "sm_sample/components/cp_joy_monitor.hpp"
+#include "sm_sample/clients/cl_joy_monitor.hpp"
 
 namespace sm_sample
 {
@@ -10,7 +10,7 @@ struct OrJoyMonitor : smacc2::Orthogonal<OrJoyMonitor>
 {
   void onInitialize() override
   {
-    this->createComponent<CpJoyMonitor>();
+    this->createClient<ClJoyMonitor>();
   }
 };
 
