652efa93e67bfb304b98f07e5e5d40c696e8e7d9
aky <akiyugata271@gmail.com>
2025-12-09 22:43:37 +0900

    update or lifecycle

--- include/sm_sample/orthogonals/or_lifecycle_nodes.hpp
+++ include/sm_sample/orthogonals/or_lifecycle_nodes.hpp
@@ -5,11 +5,21 @@
 
 using cl_lifecyclenode::ClLifecycleNode;
 
+struct ClJoy2Twist : public ClLifecycleNode
+{
+  using ClLifecycleNode::ClLifecycleNode;
+};
+
+struct ClCanBridge : public ClLifecycleNode
+{
+  using ClLifecycleNode::ClLifecycleNode;
+};
+
 struct OrLifecycleNodes : smacc2::Orthogonal<OrLifecycleNodes>
 {
     void onInitialize() override
     {
-        this->createClient<ClLifecycleNode>("joy_vel_converter");
-        this->createClient<ClLifecycleNode>("test_canbridge");
+        this->createClient<ClJoy2Twist>("joy_vel_converter");
+        this->createClient<ClCanBridge>("test_canbridge");
     }
 };
