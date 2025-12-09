7140d6c1f5b2b78655fbe1d79a398eb5d9439504
aky <akiyugata271@gmail.com>
2025-12-09 22:01:43 +0900

    buidl

--- include/sm_sample/states/st_wait_connections.hpp
+++ include/sm_sample/states/st_wait_connections.hpp
@@ -1,47 +1,31 @@
 #pragma once
 
 #include <smacc2/smacc.hpp>
-#include <sr_all_events_go/sr_all_events_go.hpp>
-#include <lifecyclenode_client/lifecyclenode_client.hpp>
-#include "sm_sample/clients/cl_joy_monitor.hpp"
-
 #include <boost/mpl/list.hpp>
 
 namespace sm_sample
 {
 
-using smacc2::state_reactors::SrAllEventsGo;
-using smacc2::state_reactors::EvAllGo;
-using cl_lifecyclenode::ClLifecycleNode;
-using cl_lifecyclenode::EvTransitionOnActivateSuccess;
-
-struct StTeleopActive;
+struct SmSample;  // 親ステートマシン
 
 namespace mpl = boost::mpl;
 
+//-----------------------------------------------
+//  StWaitConnections
+//-----------------------------------------------
 struct StWaitConnections
   : smacc2::SmaccState<StWaitConnections, SmSample>
 {
-  using SmaccState::SmaccState;
+  using Base = smacc2::SmaccState<StWaitConnections, SmSample>;
+  using Base::SmaccState;
 
-  // CAN ノードが Active になったイベント型 alias
-  using EvCanActivated =
-    EvTransitionOnActivateSuccess<ClLifecycleNode, OrLifecycleNodes>;
-
-  using reactions = mpl::list<
-    smacc2::Transition<EvAllGo, StTeleopActive>
-  >;
+  // まだ遷移は何も定義しない（あとで足す）
+  using reactions = mpl::list<>;
 
   void onEntry()
   {
-    RCLCPP_INFO(getLogger(), "StWaitConnections: waiting for Joy & CAN");
-
-    // Joy / CAN の両方が揃ったら EvAllGo を出す state reactor
-    auto * sr = this->template createStateReactor<SrAllEventsGo>();
-
-    sr->template addInputEvent<EvControllerConnected>();
-    sr->template addInputEvent<EvCanActivated>();
+    RCLCPP_INFO(this->getLogger(), "StWaitConnections: entry");
   }
 };
 
-} // namespace sm_sample
+}  // namespace sm_sample
