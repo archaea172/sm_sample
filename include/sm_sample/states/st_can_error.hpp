72ad13be1239ba36bf4611e42f2f5a26c5c8d58f
aky <akiyugata271@gmail.com>
2025-12-09 21:34:46 +0900

    add st_can_error

---
+++ include/sm_sample/states/st_can_error.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <smacc2/smacc.hpp>
+
+namespace sm_sample
+{
+
+struct StWaitConnections;
+
+struct EvReset : sc::event<EvReset> {};
+
+struct StCanError
+  : smacc2::SmaccState<StCanError, SmSample>
+{
+  using SmaccState::SmaccState;
+
+  using reactions = mpl::list<
+    smacc2::Transition<EvReset, StWaitConnections>
+  >;
+
+  void onEntry()
+  {
+    RCLCPP_ERROR(getLogger(), "StCanError: CAN error detected, teleop stopped");
+  }
+};
+
+} // namespace sm_omni_teleop
