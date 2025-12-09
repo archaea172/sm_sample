72ad850cee20b492a4bbb13ff2389367d3d9f321
aky <akiyugata271@gmail.com>
2025-12-09 21:29:35 +0900

    add st_wait_connections

---
+++ include/sm_sample/states/st_wait_connections.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <smacc2/smacc.hpp>
+#include <sr_all_events_go/sr_all_events_go.hpp>
+#include <lifecyclenode_client/lifecyclenode_client.hpp>
+#include "sm_sample/components/cp_joy_monitor.hpp"
+
+namespace sm_sample
+{
+
+using smacc2::state_reactors::SrAllEventsGo;
+using smacc2::state_reactors::EvAllGo;
+using cl_lifecyclenode::ClLifecycleNode;
+using cl_lifecyclenode::EvTransitionOnActivateSuccess;
+
+struct StTeleopActive;
+
+struct StWaitConnections
+  : smacc2::SmaccState<StWaitConnections, SmOmniTeleop>
+{
+  using SmaccState::SmaccState;
+
+  // CAN ノードが Active になったイベント型 alias
+  using EvCanActivated =
+    EvTransitionOnActivateSuccess<ClLifecycleNode, OrLifecycleNodes>;
+
+  using reactions = mpl::list<
+    smacc2::Transition<EvAllGo, StTeleopActive>
+  >;
+
+  void onEntry()
+  {
+    RCLCPP_INFO(getLogger(), "StWaitConnections: waiting for Joy & CAN");
+
+    // Joy / CAN の両方が揃ったら EvAllGo を出す state reactor
+    auto * sr = this->template createStateReactor<SrAllEventsGo>();
+
+    sr->template addInputEvent<EvControllerConnected>();
+    sr->template addInputEvent<EvCanActivated>();
+  }
+};
+
+} // namespace sm_sample
